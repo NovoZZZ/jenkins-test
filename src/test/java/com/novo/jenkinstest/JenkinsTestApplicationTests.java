617add511841d47cf0402bdf76b79ad69fd02a7a
NovoZZZ <Novo.Zou@hotmail.com>
2021-04-22 16:16:13 +0800

    Initial commit

---
+++ src/test/java/com/novo/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novo.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
