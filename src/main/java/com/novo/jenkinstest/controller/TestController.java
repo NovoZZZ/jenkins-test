3a43a86812643376bd70144dc2d2d31a72fc43cf
NovoZZZ <Novo.Zou@hotmail.com>
2021-04-23 17:16:56 +0800

    Second time to try webhook;

--- src/main/java/com/novo/jenkinstest/controller/TestController.java
+++ src/main/java/com/novo/jenkinstest/controller/TestController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping(value = "/test")
     public String test() {
-        return "2021年04月23日17:02:13 成功部署，尝试webHook";
+        return "2021年04月23日17:16:37 第二次尝试webhook";
     }
 }
